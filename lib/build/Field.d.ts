bdf4fd95506097458833812969cb3e34b51f2b25
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2020-11-10 19:09:23 +0200

    Revert "- compiled"
    This reverts commit f58d88d747973792383e0850f600b49948606500.
    
    # Conflicts:
    #	package.json

--- lib/build/Field.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import { FieldInterface } from './types';
-export declare const Field: <T extends string | number | boolean, ET = HTMLInputElement>(props: FieldInterface<T, ET>) => JSX.Element;
--- lib/build/FormContext.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { FormStateInterface } from './types';
-export declare const FormContext: import("react").Context<FormStateInterface>;
-export declare const FormProvider: import("react").Provider<FormStateInterface>;
-export declare const FormConsumer: import("react").Consumer<FormStateInterface>;
-export declare function useFormContext(): FormStateInterface;
-export default FormContext;
