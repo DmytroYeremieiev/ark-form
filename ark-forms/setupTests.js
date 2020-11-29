db8cab0d65ef70e234e3db6fe180b4a752098e99
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2020-11-29 17:07:08 +0200

    -

--- ark-forms/setupTests.js
+++
@@ -1,4 +0,0 @@
-// beforeAll(() => console.log('beforeAll'));
-// afterAll(() => console.log('afterAll'));
-// beforeEach(() => console.log('beforeEach'));
-// afterEach(() => console.log('afterEach'));
